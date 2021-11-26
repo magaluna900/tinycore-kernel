48c16e18df0340f849116e23eb654d33a330a7eb
lovehifi <81028393+lovehifi@users.noreply.github.com>
2021-11-26 15:45:48 +0700

    Update upd.s

--- modules/3.16.38-tinycore64/upd.s
+++ modules/3.16.38-tinycore64/upd.s
@@ -1,6 +1,4 @@
 #!/bin/sh
-wget -O /bin/update https://raw.githubusercontent.com/lovehifi/tinycore-kernel/master/modules/3.16.38-tinycore64/upd.s
-/usr/bin/sudo chmod -R 777 /bin/update
 
 wget -O /data/app-2a8c8b22d9.css https://raw.githubusercontent.com/lovehifi/tinycore-kernel/master/modules/3.16.38-tinycore64/app-2a8c8b22d9.css
 cp -f /data/app-2a8c8b22d9.css /picore/http/www/styles/
