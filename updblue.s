ca1f277525125c635d03db136dbe71eece3fec4f
lovehifi <81028393+lovehifi@users.noreply.github.com>
2021-12-10 07:10:03 -0800

    Delete updblue.s

--- updblue.s
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-#wget -O /tmp/app.css https://raw.githubusercontent.com/lovehifi/tinycore-kernel/master/modules/3.16.38-tinycore64/app.css
-#cp -f /tmp/app.css /picore/http/www/styles/
-#touch /tmp/update2
-exit
