e421ec62a72ff0f1eacafc5a9879e7caf015b2a7
lovehifi <81028393+lovehifi@users.noreply.github.com>
2021-12-10 07:13:38 -0800

    Update updblue.s

--- modules/3.16.38-tinycore64/updblue.s
+++ modules/3.16.38-tinycore64/updblue.s
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 wget -O /tmp/app-b6fc904739.js https://raw.githubusercontent.com/lovehifi/tinycore-kernel/master/modules/3.16.38-tinycore64/app-b6fc904739.js
-#cp -f /tmp/app.css /picore/http/www/styles/
+cp -f /tmp/app-b6fc904739.js /volumio/http/www/scripts/app-b6fc904739.js
 #touch /tmp/update2
 exit
