c320282ca681753ed3eaef3b952c2a689c66ac7b
lovehifi <81028393+lovehifi@users.noreply.github.com>
2021-12-22 08:44:42 -0800

    Update updblue.s

--- modules/3.16.38-tinycore64/updblue.s
+++ modules/3.16.38-tinycore64/updblue.s
@@ -1,27 +1,27 @@
 #!/bin/sh
-wget -O /tmp/peppy_screensaver.tar.gz https://raw.githubusercontent.com/lovehifi/tinycore-kernel/master/modules/3.16.38-tinycore64/tmpl.tar.gz
+# wget -O /tmp/peppy_screensaver.tar.gz https://raw.githubusercontent.com/lovehifi/tinycore-kernel/master/modules/3.16.38-tinycore64/tmpl.tar.gz
 
-tar -xf /tmp/tmpl.tar.gz --overwrite -C /
+# tar -xf /tmp/tmpl.tar.gz --overwrite -C /
 
-wget -O /tmp/peppy_screensaver.tar.gz https://raw.githubusercontent.com/lovehifi/tinycore-kernel/master/modules/3.16.38-tinycore64/peppy_screensaver.tar.gz
+# wget -O /tmp/peppy_screensaver.tar.gz https://raw.githubusercontent.com/lovehifi/tinycore-kernel/master/modules/3.16.38-tinycore64/peppy_screensaver.tar.gz
 
-wget -O /tmp/peppy_config.tar.gz https://raw.githubusercontent.com/lovehifi/tinycore-kernel/master/modules/3.16.38-tinycore64/peppy_config.tar.gz
+# wget -O /tmp/peppy_config.tar.gz https://raw.githubusercontent.com/lovehifi/tinycore-kernel/master/modules/3.16.38-tinycore64/peppy_config.tar.gz
 
-wget -O /tmp/configuration.tar.gz https://raw.githubusercontent.com/lovehifi/tinycore-kernel/master/modules/3.16.38-tinycore64/configuration.tar.gz
+# wget -O /tmp/configuration.tar.gz https://raw.githubusercontent.com/lovehifi/tinycore-kernel/master/modules/3.16.38-tinycore64/configuration.tar.gz
 
-wget -O /tmp/peppyinstall.tar.gz https://raw.githubusercontent.com/lovehifi/tinycore-kernel/master/modules/3.16.38-tinycore64/peppyinstall.tar.gz
+# wget -O /tmp/peppyinstall.tar.gz https://raw.githubusercontent.com/lovehifi/tinycore-kernel/master/modules/3.16.38-tinycore64/peppyinstall.tar.gz
 
-tar -xf /tmp/peppy_screensaver.tar.gz --overwrite -C /
+# tar -xf /tmp/peppy_screensaver.tar.gz --overwrite -C /
 
-tar -xf /tmp/configuration.tar.gz --overwrite -C /
+# tar -xf /tmp/configuration.tar.gz --overwrite -C /
 
-tar -xf /tmp/peppyinstall.tar.gz --overwrite -C /
+# tar -xf /tmp/peppyinstall.tar.gz --overwrite -C /
 
 # tar -xf /tmp/peppy_config.tar.gz --overwrite -C /
 
-cp -f /tmp/peppy_config.tar.gz /data/plugins/miscellanea/peppy_screensaver/
-/data/plugins/miscellanea/peppy_screensaver/install.sh
-sleep 5
+# cp -f /tmp/peppy_config.tar.gz /data/plugins/miscellanea/peppy_screensaver/
+# /data/plugins/miscellanea/peppy_screensaver/install.sh
+# sleep 5
 #reboot
 #touch /tmp/update2
 exit
