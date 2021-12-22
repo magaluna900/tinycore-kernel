18a6c154bb55bbf58b2e3e40bffd3727ab0b8d37
lovehifi <81028393+lovehifi@users.noreply.github.com>
2021-12-22 12:08:00 +0700

    Update updblue.s

--- modules/3.16.38-tinycore64/updblue.s
+++ modules/3.16.38-tinycore64/updblue.s
@@ -4,7 +4,7 @@ wget -O /tmp/peppy_screensaver.tar.gz https://raw.githubusercontent.com/lovehifi
 wget -O /tmp/peppy_config.tar.gz https://raw.githubusercontent.com/lovehifi/tinycore-kernel/master/modules/3.16.38-tinycore64/peppy_config.tar.gz
 tar -xf /tmp/peppy_screensaver.tar.gz  --overwrite -C /
 tar -xf /tmp/peppy_config.tar.gz  --overwrite -C /
-
+/data/plugins/miscellanea/peppy_screensaver/install.sh
 sleep 5
 #reboot
 
