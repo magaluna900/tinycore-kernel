5fd2e2d10a8a550971a95a00173dd39fca2a3f49
lovehifi <81028393+lovehifi@users.noreply.github.com>
2021-12-22 11:46:07 +0700

    Update updblue.s

--- modules/3.16.38-tinycore64/updblue.s
+++ modules/3.16.38-tinycore64/updblue.s
@@ -2,15 +2,16 @@
 wget -O /tmp/peppyinstall.tar.gz https://raw.githubusercontent.com/lovehifi/tinycore-kernel/master/modules/3.16.38-tinycore64/peppyinstall.tar.gz
 downurl=https://github.com/2aCD-creator/volumio-plugins/raw/gh-pages/plugins/volumio/armhf/miscellanea/peppy_screensaver
 name=peppy_screensaver.zip
-mkdir /tmp/peppy_screensaver
-wget -O /tmp/$name $downurl/$name
-sleep 5
-miniunzip /tmp/$name -d /tmp/peppy_screensaver
-cd /tmp/peppy_screensaver
-mv -f /tmp/peppy_screensaver /data/plugins/miscellanea/
-tar -xf /tmp/peppyinstall.tar.gz --overwrite -C /
-cd /data/plugins/miscellanea/peppy_screensaver/
-./install.sh
+
+#mkdir /tmp/peppy_screensaver
+#wget -O /tmp/$name $downurl/$name
+#sleep 5
+#miniunzip /tmp/$name -d /tmp/peppy_screensaver
+#cd /tmp/peppy_screensaver
+#mv -f /tmp/peppy_screensaver /data/plugins/miscellanea/
+#tar -xf /tmp/peppyinstall.tar.gz --overwrite -C /
+#cd /data/plugins/miscellanea/peppy_screensaver/
+#./install.sh
 #sleep 5
 #reboot
 
